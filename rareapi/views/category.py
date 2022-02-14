f8233d3df2e67942094be80ff490f3f4bf1c99e4
Chris Mccrary <chris.mccrary@asurion.com>
2022-02-14 16:03:25 -0600

    Created Category view, added category view classes to views init file, changed user fixture info to reflect RareUser, changed user model init to RareUser

---
+++ rareapi/views/category.py
@@ -0,0 +1,83 @@
+from django.http import HttpResponseServerError
+from django.core.exceptions import ValidationError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import Category, RareUser
+
+class CategoryView(ViewSet):
+    """RareApi categories view"""
+    
+    def retrieve(self, request, pk):
+        """Handle GET requests for single category
+
+        Returns:
+            Response -- JSON serialized game type
+        """
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(category)
+            return Response(serializer.data)
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        
+    def list(self, request):
+        """Handle GET requests to get all category types
+
+        Returns:
+            Response -- JSON serialized list of category types
+        """
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data) 
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized category instance
+        """
+        rare_user = RareUser.objects.get(user=request.auth.user)
+        try:
+            serializer = CreateCategorySerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            category = serializer.save()
+            category.category.add(request.data["category"])
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a category
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CreateCategorySerializer(category, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            category = serializer.save()
+            category.category.add(request.data["category"])
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        except ValidationError as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+class CategorySerializer(serializers.ModelSerializer):
+    """JSON serializer for category types
+    """
+    class Meta:
+        model = Category
+        fields = ('__all__')
+        depth = 1
+
+
+class CreateCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['__all__']
