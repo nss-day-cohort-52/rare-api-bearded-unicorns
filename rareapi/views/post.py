cdf4e814faa79c35f0a4b4d00835c5148fdbb5f6
FylthyPlayz <jacob_cook_football@yahoo.com>
2022-02-15 14:02:01 -0600

    whoops

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -15,40 +15,40 @@ class PostViewSet(ViewSet):
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
     def list(self, request):
-            posts = Post.objects.all()
-            category = Category.query_params.get('category', None)
-            tag = PostTag.query_params.get('tag', None)
-            if category is not None:
-                posts = posts.filter(category_id=category)
-            if tag is not None:
-                posts = posts.filter(tag_id=tag)
-            serializer = PostSerializer(posts, many=True)
-            return Response(serializer.data)
+        posts = Post.objects.all()
+        category = Category.query_params.get('category', None)
+        tag = PostTag.query_params.get('tag', None)
+        if category is not None:
+            posts = posts.filter(category_id=category)
+        if tag is not None:
+            posts = posts.filter(tag_id=tag)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
 
     def create(self, request):
-            post = Post.objects.get(user=request.auth.user)
-            try:
-                serializer = CreatePostSerializer(data=request.data)
-                serializer.is_valid(raise_exception=True)
-                serializer.save(rareuser=rareuser)
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            except ValidationError as ex:
-                return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+        post = Post.objects.get(user=request.auth.user)
+        try:
+            serializer = CreatePostSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(post=post)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk):
-            try:
-                post = Post.objects.get(pk=pk)
-                serializer = CreatePostSerializer(post, data=request.data)
-                serializer.is_valid(raise_exception=True)
-                serializer.save()
-                return Response(None, status=status.HTTP_204_NO_CONTENT)
-            except ValidationError as ex:
-                return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            post = Post.objects.get(pk=pk)
+            serializer = CreatePostSerializer(post, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        except ValidationError as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk):
-            post = Post.objects.get(pk=pk)
-            post.delete()
-            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
