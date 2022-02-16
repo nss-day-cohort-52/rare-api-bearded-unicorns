fef22b1b842d560f46388f2c99f29090d99f5fca
FylthyPlayz <jacob_cook_football@yahoo.com>
2022-02-16 11:05:49 -0600

    depth

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -54,6 +54,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('__all__')
+        depth = 3
 
 class CreatePostSerializer(serializers.ModelSerializer):
     class Meta:
